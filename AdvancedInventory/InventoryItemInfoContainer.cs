86927f2d0357479eb1b31fdee614dd06b7ac277c
Xue Bingsheng <xuebingsheng007@gmail.com>
2020-12-06 09:16:42 +0800

    u

--- AdvancedInventory/InventoryItemInfoContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace BiangStudio.AdvancedInventory
-{
-    public abstract class InventoryItemInfoContainer : MonoBehaviour
-    {
-        public abstract IInventoryItemContentInfo ItemInfo { get; }
-    }
-}
